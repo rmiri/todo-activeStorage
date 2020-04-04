6768182dfa42826736c1c6fc7112f7bdd684d730
github username <github email address>
2020-04-04 17:11:24 +0100

    one image added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -69,6 +69,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todo).permit(:task)
-    end
+      params.require(:todo).permit(:task, :picture)
+  end
 end
